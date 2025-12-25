a9fed1d1bc5ee7ae7081b30a9d79896c342c310f
unknown <krylovartem1994@gmail.com>
2025-12-25 07:06:24 +0200

    added textInput

---
+++ components/header.tsx
@@ -0,0 +1,12 @@
+import Link from "next/link";
+
+const Header = () => {
+  return (
+    <div className="flex justify-around align-baseline gap-1 w-full py-1">
+      <Link href="/">Snippets</Link>
+      <Link href="/snippets/new">Create New</Link>
+    </div>
+  );
+};
+
+export default Header;
