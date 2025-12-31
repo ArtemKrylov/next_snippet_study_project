2d068d8c47c9d6e1aee2233d2c0516c395faba92
unknown <krylovartem1994@gmail.com>
2025-12-31 18:36:31 +0200

    fixed prism config

--- db/index.ts
+++ db/index.ts
@@ -1,4 +1,4 @@
-import { PrismaClient } from "@prisma/client";
+// import { PrismaClient } from "@prisma/client";
 // export const db = new PrismaClient();
 
 // import { PrismaClient } from "@/app/generated/prisma";
@@ -17,6 +17,7 @@ import { PrismaClient } from "@prisma/client";
 //   globalForPrisma.prisma = db;
 // }
 
+import { PrismaClient } from "../generated/prisma/client";
 import "dotenv/config";
 import { PrismaBetterSqlite3 } from "@prisma/adapter-better-sqlite3";
 // import { PrismaClient } from "../generated/prisma/client";
